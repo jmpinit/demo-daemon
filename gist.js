bfb8743690e2929877bc1c7a37cabb24eeee117b
Owen Trueblood <jmptable@mit.edu>
2014-01-02 15:49:52 -0500

    Load drawing code onto display page from gist id.

--- gist.js
+++ gist.js
@@ -10,31 +10,3 @@ exports.url = url;
 exports.get = function(id, callback) {
 	rest.get('https://api.github.com/gists/'+id).on('complete', callback);
 }
-exports.create = function (content, callback) {
-	var body = 'files[newfile]=' + content;
-
-	var options = {
-		host: 'gist.github.com',
-		port: 80,
-		path: '/api/v1/json/new',
-		method: 'POST',
-		headers: {
-			'host': 'gist.github.com',
-			'Content-length': body.length, 
-			'Content-Type': 'application/x-www-form-urlencoded'
-		}
-	};
-
-	var req = http.request(options, function(res) {
-		res.body = '';
-		res.setEncoding('utf8');
-		res.on('data', function (chunk) {
-			res.body += chunk;
-		});
-		res.on('end', function () {
-			callback.apply(null, [url(res.body)]);
-		});
-	});
-
-	req.end(body);
-};
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+var http = require('http');
 var express = require('express');
 var app = express();
+
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 
 var gist = require('./gist.js');
 
@@ -7,43 +11,8 @@ app.configure(function(){
 	app.use(express.static(__dirname + '/public'));
 });
 
-/*app.get("/size/:file", function(req, res) {
-	var fn = './media/' + req.params.file;
-	fs.exists(fn, function(exists) {
-		if(exists) {
-			fs.stat(fn, function(error, stats) {
-				console.log(fn);
-				console.log(stats.size);
-
-				res.writeHead(200, { 'Content-Type': 'application/json' });
-				res.write(stats.size.toString());
-				res.end();
-			});
-		}
-	});
-});*/
-
-/*app.get("/media", function(req, res) {
-	fs.readdir('./media', function(err, files) {
-		var body = "";
-
-		if(!err) {
-			body = { files: files };
-
-			res.writeHead(200, { 'Content-Type': 'application/json' });
-			res.write(JSON.stringify(body));
-			res.end();
-
-			console.log('directory listing via /media');
-		} else {
-			res.writeHead(500, "Could not get directory listing.", {'Content-Type': 'text/html'});
-			res.end();
-		}
-	});
-});*/
-
 app.get("/add/user", function(req, res) {
-	gist.get("7836051", function(data) {console.log(data.files.makefile.content);});
+	gist.get("8225527", function(data) {console.log(data.files.makefile.content);});
 	
 	res.writeHead(200, "OK", {'Content-Type': 'text/html'});
 	console.log("OK");
@@ -51,26 +20,59 @@ app.get("/add/user", function(req, res) {
 	res.end();
 });
 
-app.put("/add/user", function(req, res) {
-	var valid = false;
+app.get("/update/:id", function(req, res) {
+	var id = req.params.id;
+	console.log("updating gist "+id);
 
-	console.log(gist.get("7836051"));
+	gist.get(id, function(code) {
+		var sourcecode = null;
+		for(var fn in code.files) {
+			sourcecode = code.files[fn].content;
+			break;
+		}
+
+		if(sourcecode != null) {
+			io.sockets.emit('code', sourcecode);
+		}
+	});
+
+	res.writeHead(200, "OK", {'Content-Type': 'text/html'});
+	console.log("OK");
+
+	res.end();
+});
+
+/*app.put("/user", function(req, res) {
+	var valid = false;
 
 	req.on('data', function(chunk) {
 		var data = JSON.parse(chunk);
 		console.log('vetting new user named ' + data.name);
 
-		//if(/* TODO user exists */)) {
+		if(true) {
 			res.writeHead(200, "OK", {'Content-Type': 'text/html'});
 			console.log("OK");
-		/*} else {
+		} else {
 			res.writeHead(400, "Bad Request", {'Content-Type': 'text/html'});
 			console.log("Bad Request");
-		}*/
+		}
 
 		res.end();
 	});
+});*/
+
+var display = null;
+io.sockets.on('connection', function(socket) {
+	var address = socket.handshake.address.address;
+	console.log("connection " + address + " accepted.");
+
+	display = socket;
+
+	socket.on('disconnect', function() {
+		display = null;
+		console.log("screen disconnected.");
+	});
 });
 
-app.listen(3000);
+server.listen(3000);
 console.log('Listening on port 3000');
